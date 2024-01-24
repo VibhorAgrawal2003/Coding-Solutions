5f4586183e873adedf6bbc390a7ac4ea8b2a2f5e
Vibhor <vibhorag2003@gmail.com>
2024-01-25 00:50:13 +0530

    code: optimal solution added for maximum container over integer array

--- leetcode/maximum_container.cpp
+++ leetcode/maximum_container.cpp
@@ -8,6 +8,60 @@ using namespace std;
 */
 
 
+// OPTIMAL SOLUTION: Accepted and beats 79% of C++ Users
+
+/***
+ * Approach: Two pointer greedy method
+ * 
+ * The idea is that we will start with maximum possible reach, assuming our walls at the ends of the vector.
+ * Then we slowly shrink the reach and for each new reach, we try to obtain the greediest sub-solution.
+ * This greediest sub-solution will always try to increase the wall length over which area is being calculated.
+ * The wall which determines the area is always the shorter wall.
+ * 
+ * As such, make two pointers traverse the array in opposite directions.
+ * At every step, shift the pointer looking at smaller wall as we want to keep taller walls in our rectangle.
+*/
+
+// Fails at:
+// Testcase: [1, 3, 2, 5, 25, 24, 5]
+
+class Solution{
+public:
+    int maxArea(vector<int>& height){
+
+        // Base case
+        if(height.size() <= 1){ return 0; }
+
+        // Two pointers
+        int p = 0;
+        int q = height.size() - 1;
+
+        // Result
+        int maxarea = 0;
+
+        while(p != q){
+
+            // Update area
+            int area = (q-p) * min(height[p], height[q]);
+            if(area > maxarea){
+                maxarea = area;
+            }
+
+            // Move pointers
+            if(height[p] <= height[q]){
+                p++;
+            }
+            else{
+                q--;
+            }
+        }
+
+        return maxarea;
+    }
+};
+
+
+
 // SUBOPTIMAL SOLUTION: Accepted but beats only 5% of C++ Users
 
 /**
@@ -21,7 +75,7 @@ using namespace std;
  * acts as the shorter side of the rectangle/area being formed.
 */
 
-class Solution{
+class Subpar{
 public:
     int maxArea(vector<int>& height){
         int i, j, maxarea = 0;
@@ -95,6 +149,7 @@ int main(){
     vector<int> height_4 = {};
     vector<int> height_5 = {0, 0, 8, 2, 1};
     vector<int> height_6 = {2, 8, 7, 1, 3};
+    vector<int> height_7 = {1, 3, 2, 5, 25, 24, 5};
 
     cout << S.maxArea(height_1) << endl;
     cout << S.maxArea(height_2) << endl;
@@ -102,9 +157,10 @@ int main(){
     cout << S.maxArea(height_4) << endl;
     cout << S.maxArea(height_5) << endl;
     cout << S.maxArea(height_6) << endl;
+    cout << S.maxArea(height_7) << endl;
 
     // Testcase Solutions
-    // 49, 1, 2, 0, 2, 9
+    // 49, 1, 2, 0, 2, 9, 15
 
     return 0;
 }
